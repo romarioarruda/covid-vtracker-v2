9c148b5f79d2752aa2e8407b48d581f970af25f9
Romário Arruda <romarioarruda98@gmail.com>
2020-05-23 00:06:21 -0300

    Ajustando AppException message

--- api/src/exceptions/AppException.php
+++ api/src/exceptions/AppException.php
@@ -3,7 +3,6 @@
 class AppException extends Exception {
 
     public function __construct($message, $code = 0, $previous = null) {
-        exit($message);
         parent::__construct($message, $code, $previous);
     }
 }
